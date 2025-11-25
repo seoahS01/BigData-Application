8ae543d8106ae367e28b7e911e8269bb83dfabbb
Nam Yeonjeong <nyj9800@ewhain.net>
2025-11-25 16:40:19 +0900

    db_connect에 담당자 주석 추가

--- team14/db_connect.php
+++ team14/db_connect.php
@@ -1,16 +1,11 @@
 <?php
-// 설정 파일 불러오기
+// 담당자 2176122 남연정
+
 require_once "db_config.php";
-
-// MySQLi 연결 생성
 $conn = mysqli_connect($db_host, $db_user, $db_password, $db_name, $db_port);
-
-// 연결 실패 시 에러 출력
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
-
-// UTF-8 설정 (한국어 데이터 사용 대비)
 mysqli_set_charset($conn, "utf8");
 
 ?>
